4c830e60e1ea296ab0e5ef597eb6022d412cf0de
olafpedda <felixwentzel@gmail.com>
2020-02-12 12:10:49 +0100

    Day night cycle depending on Daylength

---
+++ Assets/Sun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+	private int dayLength;
+	public float speedMult = 1;
+
+	private void Start()
+	{
+		dayLength = GameManager.instance.dayLength;
+	}
+	void Update()
+    {
+		transform.RotateAround(Vector3.zero, Vector3.right, (180/dayLength) * Time.deltaTime );
+		transform.LookAt(Vector3.zero);
+    }
+}
